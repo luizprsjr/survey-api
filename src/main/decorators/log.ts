7c64e76555822940b82dc43823fef82feec23956
Luiz Pires <lpiresjunior@gmail.com>
2023-08-12 06:41:14 -0300

    refactor: move data layer interfaces to folders

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path'
 
-import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
 import {
   Controller,
   HttpRequest,
