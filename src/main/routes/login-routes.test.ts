58b316b6719279ce85e6430c0519278c85ae2a21
Luiz Pires <lpiresjunior@gmail.com>
2023-08-13 07:56:04 -0300

    test: ensure login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    it('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'lpiresjunior@gmail.com',
+          password: '123',
+        })
+        .expect(401)
+    })
   })
 })
