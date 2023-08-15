32188d166e65b48ba6e29b3ce78a029ebe3f33af
Luiz Pires <lpiresjunior@gmail.com>
2023-08-15 06:16:09 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.ts
@@ -0,0 +1,15 @@
+import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
+import { Validation } from '../../../../presentation/protocols'
+import {
+  EmailValidation,
+  RequiredFieldValidation,
+  ValidationComposite,
+} from '../../../../validation/validators'
+
+export const makeAddSurveyValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['question', 'answers']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  return new ValidationComposite(validations)
+}
