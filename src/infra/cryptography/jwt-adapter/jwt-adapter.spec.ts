16afce659b5a65dda70d9a6afc50d71ab19e1936
Luiz Pires <lpiresjunior@gmail.com>
2023-08-16 03:25:08 -0300

    test: ensure jwtAdapter call verify with correct values

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -6,6 +6,10 @@ jest.mock('jsonwebtoken', () => ({
   async sign(): Promise<string> {
     return new Promise((resolve) => resolve('any_token'))
   },
+
+  async verify(): Promise<string> {
+    return new Promise((resolve) => resolve('any_value'))
+  },
 }))
 
 const makeSut = (): JwtAdapter => {
@@ -34,6 +38,15 @@ describe('Jwt Adapter', () => {
       })
       const promise = sut.encrypt('any_id')
       await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('verify()', () => {
+    it('should call verify with correct values', async () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt('any_token')
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
   })
 })
